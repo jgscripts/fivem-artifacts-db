a7e398bd410502ab40842b744bb3e90660b80e3a
alp1x <hkey.yea@gmail.com>
2024-10-16 07:10:32 +0300

    css refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,7 @@ import { getRecommendedArtifact } from "@/actions/fivem";
 import artifactDb from "@/db.json";
 
 export default async function Home() {
-  const data:
-    | {
-        windowsDownloadLink: string;
-        linuxDownloadLink: string;
-        recommendedArtifact: string;
-      }
-    | false = await getRecommendedArtifact();
+  const data = await getRecommendedArtifact();
 
   if (!data)
     return (
@@ -21,40 +15,39 @@ export default async function Home() {
     <div className="max-w-[500px] min-w-[300px] p-5 mx-auto">
       <h1 className="text-2xl font-bold mb-5">FiveM Artifacts DB</h1>
 
-      <div className="border-green-500 border-2 p-3 px-4 mb-5 text-lg">
+      <div className="border border-zinc-800 bg-zinc-900 p-3 px-4 mb-5 text-lg rounded text-center">
         <p>
           <span>
-            Latest<sup>*</sup> artifact with no reported issues
+            Latest<sup className="text-blue-400 font-bold">*</sup> artifact with no reported issues
           </span>
-          <code className="bg-green-500 p-1 px-2 ml-2 text-white font-bold">
+          <code className="bg-green-500 p-1 px-2 ml-2 font-bold rounded font-sans text-base text-white">
             {data.recommendedArtifact}
           </code>
         </p>
-        <div className="flex gap-2">
-          <p>Download:</p>
-          <a
-            href={data.windowsDownloadLink}
-            className="text-blue-500 underline"
-          >
+        <div className="flex gap-2 justify-center text-base">
+          <p className="font-semibold">Download:</p>
+          <a href={data.windowsDownloadLink} className="text-blue-500 hover:underline">
             Windows
           </a>
-          <a href={data.linuxDownloadLink} className="text-blue-500 underline">
+          <a href={data.linuxDownloadLink} className="text-blue-500 hover:underline">
             Linux
           </a>
         </div>
       </div>
 
-      <div className="text-xs text-gray-400 my-4">
-        * Note: There is a wait period of ~1 day before the very newest artifact
-        is recommended, to allow time for issues to be reported.
+      <div className="text-xs my-4 border border-zinc-800 p-2.5 rounded bg-zinc-900">
+        <span className="bg-blue-500 font-semibold px-0.5 rounded-s-full mr-1 border border-opacity-30 border-zinc-900">
+          *Note:
+        </span>
+        There is a wait period of ~1 day before the very newest artifact is recommended, to allow time for issues to be reported.
       </div>
 
-      <div className="flex justify-between mb-2 text-xs">
+      <div className="flex justify-between mt-10 mb-2 text-xs">
         <p className="text-gray-400">Artifacts with reported issues:</p>
         <a
           href="https://github.com/jgscripts/fivem-artifacts-db/tree/main?tab=readme-ov-file#contributing"
           target="_blank"
-          className="underline text-blue-500 text-right"
+          className="underline text-blue-500"
         >
           Know an issue? Let us know!
         </a>
@@ -63,12 +56,12 @@ export default async function Home() {
       {Object.entries(artifactDb.brokenArtifacts)
         .sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
         .map(([key, value]) => (
-          <div key={key} className="border border-gray-500 p-2 mb-2 text-sm">
+          <div key={key} className="border border-zinc-800 p-2 mb-3 bg-zinc-900 rounded cursor-pointer">
             <code className="leading-5">
-              <span className="bg-red-500 p-1 px-2 mr-2 text-white font-bold">
+              <span className="text-xs font-sans bg-red-500 p-1 px-1 mr-1 font-bold rounded border border-opacity-30 border-zinc-900">
                 {key}
               </span>
-              <span className="text-gray-300">{value}</span>
+              <span className="text-sm font-sans">{value}</span>
             </code>
           </div>
         ))}
